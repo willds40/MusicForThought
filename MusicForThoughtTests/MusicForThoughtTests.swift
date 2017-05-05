361d7d034e199a86795ac200f2a7eb04bc26fe86
wdsand <wdsand@thoughtworks.com>
2017-05-05 11:48:06 -0400

    carthage added and working

--- MusicForThoughtTests/MusicForThoughtTests.swift
+++ MusicForThoughtTests/MusicForThoughtTests.swift
@@ -6,31 +6,12 @@
 //  Copyright © 2017 Will Devon-Sand. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 @testable import MusicForThought
 
-class MusicForThoughtTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class MusicForThoughtTests: QuickSpec{
+    override func spec() {
+    expect(true).to(beTrue())
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
