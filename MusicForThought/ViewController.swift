687c302b102b30c544ac67fb0b2c733470f57c41
wdsand <wdsand@thoughtworks.com>
2017-05-05 11:32:29 -0400

    initial commit

---
+++ MusicForThought/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicForThought
+//
+//  Created by Will Devon-Sand on 5/5/17.
+//  Copyright © 2017 Will Devon-Sand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
