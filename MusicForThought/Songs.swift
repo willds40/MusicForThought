863e9504058e8612a58511199dd2b4a76a4f06a9
wdsand <wdsand@thoughtworks.com>
2017-05-11 13:56:50 -0400

    added methods to retrieve rap and rock songs

--- MusicForThought/Songs.swift
+++ MusicForThought/Songs.swift
@@ -1,17 +1,22 @@
 class Songs{
-
-//Rap Songs
+    
+    //Rap Songs
     var encore = ["id":1, "name":"99Problems", "Description":"Do you want more?", "Cover Art":"www.IOwnBrooklyn.com"] as [String : Any]
     var workoutPlan = ["id":2, "name":"WokoutPlan", "Description":"Get Fit On Kayne's plan", "Cover Art": "www.thisiskayne.com"] as [String : Any]
-
-//Rock Songs
+    
+    func getRapSongs()->[[String:Any]]{
+        let rapSongs = [encore,workoutPlan]
+        return rapSongs
+    }
+    
+    //Rock Songs
     var help = ["id":3, "name":"Help!", "Description":"I need somebody, pleaseeee...", "Cover Art":"www.BeatlesForever.com"] as [String : Any]
     
     var imagine = ["id":4, "name":"Imagine", "Description":"World Peace", "Cover Art":"www.IAmStillAlive.com"] as [String : Any]
-
     
+    func getRockSongs()->[[String:Any]]{
+        let rockSongs = [help,imagine]
+        return rockSongs
+    }
     
-    
-    
-
 }
