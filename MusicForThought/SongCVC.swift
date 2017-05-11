0551e8040208180f41205cb972c5fbede5f67e2f
wdsand <wdsand@thoughtworks.com>
2017-05-11 16:13:38 -0400

    correct song id is being passed to description vc

--- MusicForThought/SongCVC.swift
+++ MusicForThought/SongCVC.swift
@@ -5,6 +5,7 @@ import UIKit
 class SongCVC: UICollectionViewController {
     var songVM:SongsVM?
     var searchTerm = ""
+    var selectedRow = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         songVM = SongsVM()
@@ -12,6 +13,7 @@ class SongCVC: UICollectionViewController {
         self.collectionView?.delegate = self
         self.collectionView?.dataSource = self
         NotificationCenter.default.addObserver(self, selector: #selector(reloadCollectionView), name: NSNotification.Name(rawValue: "reloadCollectionView"), object: nil)
+        collectionView?.isUserInteractionEnabled = true
    }
     
     func reloadCollectionView(){
@@ -25,7 +27,18 @@ class SongCVC: UICollectionViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var songID = 0.0
+        let song   = songVM?.songs[selectedRow]
+        
+        if let key = song?.keys.filter({ $0.lowercased().contains("id") }).first, let id = song?[key] {
+           songID  = id as! Double
         }
+        if segue.identifier == "DescriptionSegue" {
+            if let descriptionTVC = segue.destination as? DescriptionTVC {
+                descriptionTVC.searchTermBySongId = songID
+            }
+        }
+    }
  
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -42,7 +55,6 @@ class SongCVC: UICollectionViewController {
         cell.layer.borderColor = UIColor.black.cgColor
         cell.backgroundColor = UIColor.white
         let song   = songVM?.songs[indexPath.row]
-        //find the value of dictionary when key === name
         if let key = song?.keys.filter({ $0.lowercased().contains("name") }).first, let title = song?[key] {
            cell.songTitleLabel.text = title as? String
         }
@@ -55,6 +67,17 @@ class SongCVC: UICollectionViewController {
     }
     
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedRow = indexPath.row
+        self.performSegue(withIdentifier: "DescriptionSegue", sender: self)
+    }
+    
+    
+    
+//    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: IndexPath) {
+//        selectedRow = indexPath.row
+//        self.performSegue(withIdentifier: "DescriptionSegue", sender: self)
+//    }
 
     /*
     // Uncomment this method to specify if the specified item should be selected
