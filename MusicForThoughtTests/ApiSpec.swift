cbc30fd4e974bd8ff64bc5e7d513599cb4afe2b1
wdsand <wdsand@thoughtworks.com>
2017-06-12 13:05:24 -0400

    api tests for json data

--- MusicForThoughtTests/ApiSpec.swift
+++ MusicForThoughtTests/ApiSpec.swift
@@ -3,17 +3,22 @@ import Nimble
 import SwiftyJSON
 @testable import MusicForThought
 class ApiSpec :QuickSpec{
+    var songPath = "/Users/wdsand/Documents/Xcode/MusicForThought/SongData.JSON"
     override func spec() {
-         var api:API!
-            beforeEach {
-                api = API()
-            }
-         describe("search request function"){
+        describe("search request function"){
+            let api = API ()
             context("endpoint is string"){
                 it("url should equal a string'"){
-            _ = api?.dummySearchRequestIfAPIExisted(forPath: "test")
-                    expect(api?.url).to(equal("test"))
+            _ = api.dummySearchRequestIfAPIExisted(forPath: "test")
+                    expect(api.url).to(equal("test"))
                 }
+            }
+        }
+        describe("retrieve data method"){
+            let api = API()
+            let jsonObj = api.retrieveData(forPath: self.songPath)
+            it ("should return a json object"){
+        expect(jsonObj).to(beAKindOf(JSON.self))
             }
         }
     }
