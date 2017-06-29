e271a190a6594c199ad5447be55afad07e5a5c12
wdsand <wdsand@thoughtworks.com>
2017-06-29 11:15:42 -0400

    data added without internet

--- MusicForThought/API.swift
+++ MusicForThought/API.swift
@@ -11,9 +11,20 @@ class API{
     
     func retrieveData(forPath path: String, completion : @escaping (JSON) -> ()){
         Alamofire.request("http://localhost:4545/" + "\(path)").responseJSON { response in
-            let jsonData = JSON(data: response.data!)
+            var jsonData = JSON(data: response.data!)
+            if jsonData.isEmpty {jsonData = self.returnMockDataWhenNoInternnet(path)}
             completion(jsonData)
             
+        }
+    }
+    
+    private func returnMockDataWhenNoInternnet(_ path:String)->JSON{
+        switch (path){
+        case "categories": return NoInternetData().getCategories()
+        case "genres": return NoInternetData().getGenres()
+        case "songs": return NoInternetData().getSongs()
+        default: return JSON.null
+       
         }
     }
     
