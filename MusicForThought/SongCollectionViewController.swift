b5af863591226d63aab8692b7e64727b3120773d
wdsand <wdsand@thoughtworks.com>
2017-07-05 10:13:41 -0400

    refactr song cvc cell styling

--- MusicForThought/SongCollectionViewController.swift
+++ MusicForThought/SongCollectionViewController.swift
@@ -8,7 +8,7 @@ class SongCVC: UICollectionViewController {
     var selectedRow = 0
     var songsAssociatedWithGenre = [Int](){
         didSet {
-        songVM.songsAsscoaitedWithTheGenre = songsAssociatedWithGenre
+            songVM.songsAsscoaitedWithTheGenre = songsAssociatedWithGenre
         }
     }
     override func viewDidLoad() {
@@ -35,20 +35,26 @@ class SongCVC: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return (songVM.songs.count)
     }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let borderColor: CGColor! = UIColor.black.cgColor
         let borderWidth: CGFloat = 1
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SongCell",
-                                                      for: indexPath) as! SongCell
+                                                      for: indexPath) as!SongCell
+        styleCell(cell: cell, borderColor: borderColor, borderWith: borderWidth, indexPath: indexPath)
+        return cell
+    }
+    
+    private func styleCell(cell: SongCell, borderColor:CGColor, borderWith: CGFloat, indexPath:IndexPath){
         cell.layer.borderColor = UIColor.black.cgColor
         cell.backgroundColor = UIColor.white
         let song   = songVM.songs[indexPath.row]
         cell.songTitleLabel.text = song.songTitle
         let itunesLogo = UIImage(named:"itunes.jpg")
         cell.image.image = itunesLogo
-        cell.image.layer.borderWidth = borderWidth
+        cell.image.layer.borderWidth = borderWith
         cell.image.layer.borderColor = borderColor
-        return cell
+        
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -57,9 +63,9 @@ class SongCVC: UICollectionViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            song   = songVM.songs[selectedRow]
         if segue.identifier == "DescriptionSegue" {
             if let descriptionTVC = segue.destination as? DescriptionTVC {
+                song   = songVM.songs[selectedRow]
                 descriptionTVC.song = song
             }
         }
